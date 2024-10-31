4deacd428aa11382a07cf109dbc7dc31d12267fa
dungnt0708 <dungnt0708.me@gmail.com>
2024-10-31 15:03:18 +0700

    add BMI

--- SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/Controllers/HomeController.cs
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
 using SEP490_G87_Vita_Nutrient_System_Client.Models;
+using Microsoft.AspNetCore.Http;
 
 
 namespace SEP490_G87_Vita_Nutrient_System_Client.Controllers
@@ -258,9 +259,128 @@ namespace SEP490_G87_Vita_Nutrient_System_Client.Controllers
         /// Dũng
         ////////////////////////////////////////////////////////////
         ///
+        public IActionResult NutritionCheck()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult NutritionCheck(DateTime birthDate, double weight, double height, string gender)
+        {
+            int age = DateTime.Today.Year - birthDate.Year;
+            if (birthDate > DateTime.Today.AddYears(-age)) age--;
 
+            if (age < 19)
+            {
+                ViewBag.AgeError = "Phần mềm này chỉ áp dụng cho người từ 19 tuổi trở lên. Vui lòng kiểm tra lại thông tin.";
+                return View();
+            }
+            double bmi = weight / Math.Pow(height / 100, 2);
+            string status;
+            string evaluation;
 
+            // Tùy chỉnh đánh giá BMI theo giới tính
+            if (gender == "Nam")
+            {
+                if (bmi < 16)
+                {
+                    status = "GẦY ĐỘ III";
+                    evaluation = "<16Kg/m²";
+                }
+                else if (bmi < 17)
+                {
+                    status = "GẦY ĐỘ II";
+                    evaluation = "16Kg/m² - <17Kg/m²";
+                }
+                else if (bmi < 18.5)
+                {
+                    status = "GẦY ĐỘ I";
+                    evaluation = "17Kg/m² - <18.5Kg/m²";
+                }
+                else if (bmi < 23)
+                {
+                    status = "BÌNH THƯỜNG";
+                    evaluation = "18.5Kg/m² - <23Kg/m²";
+                }
+                else if (bmi < 25)
+                {
+                    status = "THỪA CÂN";
+                    evaluation = "23Kg/m² - <25Kg/m²";
+                }
+                else if (bmi < 30)
+                {
+                    status = "BÉO PHÌ ĐỘ I";
+                    evaluation = "25Kg/m² - <30Kg/m²";
+                }
+                else if (bmi < 35)
+                {
+                    status = "BÉO PHÌ ĐỘ II";
+                    evaluation = "30Kg/m² - <35Kg/m²";
+                }
+                else
+                {
+                    status = "BÉO PHÌ ĐỘ III";
+                    evaluation = "≥35Kg/m²";
+                }
+            }
+            else // Giả định là giới tính "Nữ"
+            {
+                if (bmi < 15.5)
+                {
+                    status = "GẦY ĐỘ III";
+                    evaluation = "<15.5Kg/m²";
+                }
+                else if (bmi < 16.5)
+                {
+                    status = "GẦY ĐỘ II";
+                    evaluation = "15.5Kg/m² - <16.5Kg/m²";
+                }
+                else if (bmi < 18)
+                {
+                    status = "GẦY ĐỘ I";
+                    evaluation = "16.5Kg/m² - <18Kg/m²";
+                }
+                else if (bmi < 22.5)
+                {
+                    status = "BÌNH THƯỜNG";
+                    evaluation = "18Kg/m² - <22.5Kg/m²";
+                }
+                else if (bmi < 24.5)
+                {
+                    status = "THỪA CÂN";
+                    evaluation = "22.5Kg/m² - <24.5Kg/m²";
+                }
+                else if (bmi < 29.5)
+                {
+                    status = "BÉO PHÌ ĐỘ I";
+                    evaluation = "24.5Kg/m² - <29.5Kg/m²";
+                }
+                else if (bmi < 34.5)
+                {
+                    status = "BÉO PHÌ ĐỘ II";
+                    evaluation = "29.5Kg/m² - <34.5Kg/m²";
+                }
+                else
+                {
+                    status = "BÉO PHÌ ĐỘ III";
+                    evaluation = "≥34.5Kg/m²";
+                }
+            }
+
+            var result = new BMIResult
+            {
+                BMI = Math.Round(bmi, 2),
+                Status = status,
+                Evaluation = evaluation,
+                Gender = gender
+            };
+            ViewBag.BirthDate = birthDate.ToString("yyyy-MM-dd");
+            ViewBag.Weight = weight;
+            ViewBag.Height = height;
+            ViewBag.Gender = gender;
+
+            return View("NutritionCheck", result);
+        }
 
 
 
