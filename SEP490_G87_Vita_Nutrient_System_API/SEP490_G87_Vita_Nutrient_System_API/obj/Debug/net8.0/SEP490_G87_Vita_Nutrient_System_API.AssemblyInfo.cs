9ddd43fc1ecc64546b7de8e25e732aac42e14b66
MinhTanTMT <132992493+MinhTanTMT@users.noreply.github.com>
2024-10-31 16:55:36 +0700

    CommitCode: 576942

--- SEP490_G87_Vita_Nutrient_System_API/SEP490_G87_Vita_Nutrient_System_API/obj/Debug/net8.0/SEP490_G87_Vita_Nutrient_System_API.AssemblyInfo.cs
+++ SEP490_G87_Vita_Nutrient_System_API/SEP490_G87_Vita_Nutrient_System_API/obj/Debug/net8.0/SEP490_G87_Vita_Nutrient_System_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEP490_G87_Vita_Nutrient_System_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2ff21cdb522b03d4eac6920856670c07f017ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f20d4f2a1fbd41d43436ca50834c2634cb0cb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEP490_G87_Vita_Nutrient_System_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEP490_G87_Vita_Nutrient_System_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
