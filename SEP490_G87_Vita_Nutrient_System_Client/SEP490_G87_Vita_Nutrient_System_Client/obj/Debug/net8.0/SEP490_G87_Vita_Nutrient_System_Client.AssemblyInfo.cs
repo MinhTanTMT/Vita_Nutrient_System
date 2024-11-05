55b0829832cc8aecf8bc9bbb34e8ffa2a21cc341
MinhTanTMT <132992493+MinhTanTMT@users.noreply.github.com>
2024-11-05 15:09:04 +0700

    CodeCommit: 687954

--- SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/obj/Debug/net8.0/SEP490_G87_Vita_Nutrient_System_Client.AssemblyInfo.cs
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/obj/Debug/net8.0/SEP490_G87_Vita_Nutrient_System_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEP490_G87_Vita_Nutrient_System_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874c20e7fccc7fc16494132f1ffe6c759947c64d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06d0a4b7c3d9db2822b2de86b4c714d822abfea")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEP490_G87_Vita_Nutrient_System_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEP490_G87_Vita_Nutrient_System_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
