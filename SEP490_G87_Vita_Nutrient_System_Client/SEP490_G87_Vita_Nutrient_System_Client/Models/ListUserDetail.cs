d0a14a1a0f524f0c5442693ec3b0555c180fb2ec
Tung <143211873+Ngomanhtung123@users.noreply.github.com>
2024-11-09 02:22:33 +0700

    update api

---
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/Models/ListUserDetail.cs
@@ -0,0 +1,17 @@
+﻿namespace SEP490_G87_Vita_Nutrient_System_Client.Models
+{
+    public class ListUserDetail
+    {
+        public int UserId { get; set; }
+        public string DescribeYourself { get; set; }
+        public int? Height { get; set; }
+        public int? Weight { get; set; }
+        public int? Age { get; set; }
+        public string WantImprove { get; set; }
+        public string UnderlyingDisease { get; set; }
+        public string InforConfirmGood { get; set; }
+        public string InforConfirmBad { get; set; }
+        public bool? IsPremium { get; set; }
+    }
+
+}
