0158735673f4f645ca69619a1113f963994e8792
SonDV <son8h123@gmail.com>
2024-11-23 12:50:31 +0700

    add update user info

---
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/wwwroot/js/userprofile.js
@@ -0,0 +1,10 @@
+﻿function openTab(event, tabId) {
+    var tabButtons = document.querySelectorAll(".tab-button");
+    var tabContents = document.querySelectorAll(".tab-content");
+
+    tabButtons.forEach(button => button.classList.remove("active"));
+    tabContents.forEach(content => content.classList.remove("active"));
+
+    event.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
