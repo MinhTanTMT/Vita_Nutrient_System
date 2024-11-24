82b85665e4079f0d38dd492ecd0bd17f5da07470
SonDV <son8h123@gmail.com>
2024-11-24 10:15:00 +0700

    user change password

--- SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/wwwroot/js/userprofile.js
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/wwwroot/js/userprofile.js
@@ -8,3 +8,21 @@
     event.currentTarget.classList.add("active");
     document.getElementById(tabId).classList.add("active");
 }
+
+// Open Modal Function
+function openChangePassModal() {
+    document.getElementById("modalOverlay").style.display = "flex";
+}
+
+// Close Modal Function
+function closeModal() {
+    document.getElementById("modalOverlay").style.display = "none";
+}
+
+// Close modal when clicking outside
+window.onclick = function (event) {
+    const modalOverlay = document.getElementById("modalOverlay");
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+}
