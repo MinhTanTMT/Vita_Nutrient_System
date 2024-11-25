d75b0b597385b8d30b31593f75376bd338a3d082
MinhTanTMT <132992493+MinhTanTMT@users.noreply.github.com>
2024-11-25 21:09:43 +0700

    CodeCommit: 875685

---
+++ SEP490_G87_Vita_Nutrient_System_API/SEP490_G87_Vita_Nutrient_System_API/Dtos/ListFoodAndDiseaseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace SEP490_G87_Vita_Nutrient_System_API.Dtos
+{
+    public class ListFoodAndDiseaseDTO
+    {
+        public int FoodListId { get; set; }
+        public int ListOfDiseasesId { get; set; }
+        public string? DiseaseName { get; set; }
+        public string? DiseaseDescribe { get; set; }
+        public string? FoodName { get; set; }
+        public string? FoodDescribe { get; set; }
+        public string? Describe { get; set; }
+        public bool? IsGoodOrBad { get; set; }
+    }
+}
---
+++ SEP490_G87_Vita_Nutrient_System_Client/SEP490_G87_Vita_Nutrient_System_Client/Models/FoodListResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace SEP490_G87_Vita_Nutrient_System_Client.Models
+{
+    public class Food
+    {
+        public int FoodListId { get; set; }
+        public int FoodTypeId { get; set; }
+        public int KeyNoteId { get; set; }
+        public int CookingDifficultyId { get; set; }
+        public string Name { get; set; }
+        public string Describe { get; set; }
+        public double Rate { get; set; }
+        public int NumberRate { get; set; }
+        public string UrlImage { get; set; }
+        public string FoodTypeName { get; set; }
+        public string KeyNoteName { get; set; }
+        public bool IsActive { get; set; }
+        public int PreparationTime { get; set; }
+        public int CookingTime { get; set; }
+        public string CookingDifficultyName { get; set; }
+    }
+}
