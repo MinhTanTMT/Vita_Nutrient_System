b30bd3471f0a588df314f327f32bab1c1b22f40c
SonDV <son8h123@gmail.com>
2024-12-05 00:28:58 +0700

    update change password

--- SEP490_G87_Vita_Nutrient_System_API/SEP490_G87_Vita_Nutrient_System_API/Models/Sep490G87VitaNutrientSystemContext.cs
+++ SEP490_G87_Vita_Nutrient_System_API/SEP490_G87_Vita_Nutrient_System_API/Models/Sep490G87VitaNutrientSystemContext.cs
@@ -83,7 +83,7 @@ public partial class Sep490G87VitaNutrientSystemContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server =localhost; database = SEP490_G87_VitaNutrientSystem_StandDataNoUser;uid=sa;pwd=admin;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("server =localhost; database = SEP490_G87_VitaNutrientSystem;uid=sa;pwd=admin;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
